cea87f53c0325be2447e1a892640e5b4e654eecc
Artem <artsmg92@gmail.com>
2022-08-26 18:39:53 +0300

    Задача №13

---
+++ Задача № 13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: "); 
+string number = Console.ReadLine();
+
+char[] numberArray = number.ToCharArray();
+if (numberArray.Length >= 3)
+{
+    Console.WriteLine($"Третья цифра : {numberArray[2]}");
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
