d5a1517254fd3c451f187ebcdf3b1b64519f1d73
Artem <artsmg92@gmail.com>
2022-08-26 18:18:15 +0300

    Второй вариант решения задачи №10

---
+++ Задача №10 вариант2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+Console.Write("Введите трехзначное число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+int result = (num % 100) / 10;
+Console.WriteLine($"Второе число равно : {result}" );
